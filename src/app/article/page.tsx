e8228ca48627403a687536ef7f397208288ddc22
Eriquy <eriquy@gmail.com>
2024-10-21 12:28:32 -0300

    feat: structure blog post

--- src/app/article/page.tsx
+++ src/app/article/page.tsx
@@ -1,14 +1,20 @@
 import BlogHeaderImage from "@/components/blogHeaderImage";
+import BlogPost from "@/components/blogPost";
 import ContactForm from "@/components/contactForm";
+import Footer from "@/components/footer";
 import Header from "@/components/header";
 
 
 export default function Article() {
   return (
-    <>
+    <div className="min-h-screen">
       <Header />
       <BlogHeaderImage />
-      <ContactForm />
-    </>
+      <div className="flex mt-10 p-4 max-w-[1216px] justify-between mx-auto">
+        <BlogPost />
+        <ContactForm />
+      </div>
+      <Footer />
+    </div>
   )
 }
